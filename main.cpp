83d9bd996dc35a8e52f86851bae5086a6689c111
courtneyjchen <92234626+courtneyjchen@users.noreply.github.com>
2022-01-10 18:06:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,15 @@
 using namespace std;
 const int capacity = 100;
 
-// Parses in top_songs_2019.csv using commas as the delimiter,
-// stores the strings in their respective arrays, calls the menu function
+// Parses in top_songs_2019.csv, stores the strings in their respective arrays, and calls the menu function.
+
 int main() {
     // Enables special characters
     setlocale(LC_ALL, "");
 
     ifstream inFS;
     string line;
-    string quoteDummy;
-    string commaDummy;
+    string dummyString;
     int i = 0;
     string songs[capacity], artists[capacity], albums[capacity], duration[capacity];
 
@@ -24,34 +23,35 @@ int main() {
         return 1;
     }
 
+    // If quote qualifiers are found, uses the next quote as the delimiter
+    // and puts excess text into dummy strings.
+    // Else, uses the comma as the delimiter.
+    
     getline(inFS, line);
     while(getline(inFS, line)){
         stringstream ss(line);
-        // If quote qualifiers are found, uses the next quote as the delimiter
-        // and puts excess text into dummy strings.
-        // Else, uses the comma as the delimiter.
         if(ss.peek() == '"') {
-            getline(ss,quoteDummy,'"');
+            getline(ss,dummyString,'"');
             getline(ss,songs[i],'"');
-            getline(ss, commaDummy, ',');
+            getline(ss, dummyString, ',');
         }
         else {
             getline(ss,songs[i],',');
         }
 
         if(ss.peek() == '"') {
-            getline(ss,quoteDummy,'"');
+            getline(ss,dummyString,'"');
             getline(ss,artists[i],'"');
-            getline(ss, commaDummy, ',');
+            getline(ss, dummyString, ',');
         }
         else {
             getline(ss,artists[i],',');
         }
 
         if(ss.peek() == '"') {
-            getline(ss,quoteDummy,'"');
+            getline(ss,dummyString,'"');
             getline(ss,albums[i],'"');
-            getline(ss, commaDummy, ',');
+            getline(ss, dummyString, ',');
         }
         else {
             getline(ss,albums[i],',');
@@ -60,8 +60,6 @@ int main() {
 
         i++;
     }
-    cout << "size is " << sizeof(songs)/sizeof(songs[0]) << endl;
-
     int choice = menu(songs, artists, albums, duration);
     while(choice != 5){
         choice = menu(songs, artists, albums, duration);
